8ce7ab94346ca8f233bffb78dac12cef7178a106
dhaqui <49834807+dhaqui@users.noreply.github.com>
2025-01-30 14:38:42 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,18 @@
+app.get("/", async (req, res) => {
+const clientId = process.env.PAYPAL_CLIENT_ID;
+res.render("checkout", {
+clientId,
+});
+});
+import * as paypal from "./paypal-api.js";
+// create order
+app.post("/api/orders", async (req, res) => {
+  const order = await paypal.createOrder(req.body.paymentSource);
+  res.json(order);
+});
+
+
+
 const app = express();
 
 // ルーティングなどの設定はここにあるはず
